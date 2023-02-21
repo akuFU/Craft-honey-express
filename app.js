783cb998d0482d8fc818581744828f52dfb2fb82
akuFU <nikita.akulenko95@gmail.com>
2023-02-22 01:47:44 +0600

    setting up routes

--- app.js
+++ app.js
@@ -1,21 +1,45 @@
 const history = require('connect-history-api-fallback');
-
 const express = require('express');
 const path = require('path');
+
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('testdb.db');
+
 const app = express(),
 	bodyParser = require("body-parser");
-	port = 3080;
+	port = process.env.PORT || 3080;
 
 app.use(bodyParser.json());
+
+app.get('/api/productsList', async (req,res) => {
+
+	let data = {};
+	db.all("SELECT * FROM products", function(err, rows) {
+
+		rows.forEach((row) => data[row.id] = row);
+		res.json(data);
+
+	}); 
+
+})
+
+app.get('/api/productsListOpt', async (req,res) => {
+
+	let data = {};
+	db.all("SELECT * FROM productsOpt", function(err, rows) {
+
+		rows.forEach((row) => data[row.id] = row);
+		res.json(data);
+
+	}); 
+
+})
+
 app.use(history());
 app.use(express.static(path.join(__dirname, './Craft-honey/dist')));
 
-app.get('/', (req,res) => {
-  res.sendFile(path.join(__dirname, './Craft-honey/dist/index.html'));
-});
-
 app.listen(port, () => {
 
-	console.log("hello world!")
+	console.log("hellofe world!")
 
 })
