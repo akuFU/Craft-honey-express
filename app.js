09faa2cde70f3d003072c86fd7c7af4376fc4194
akuFU <nikita.akulenko95@gmail.com>
2023-02-21 19:29:14 +0600

    remove test code

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
-var http = require('http');
-var history = require('connect-history-api-fallback');
-var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('testdb.db');
+const history = require('connect-history-api-fallback');
 
 const express = require('express');
 const path = require('path');
@@ -16,16 +13,6 @@ app.use(express.static(path.join(__dirname, './Craft-honey/dist')));
 app.get('/', (req,res) => {
   res.sendFile(path.join(__dirname, './Craft-honey/dist/index.html'));
 });
-
-db.run("CREATE TABLE IF NOT EXISTS test (id INTEGER PRIMARY KEY, name TEXT)");
-
-db = new sqlite3.Database('testdb.db');
-
-function add() {
-
-	db.run("INSERT INTO test (name) VALUES ('john')");
-
-}
 
 app.listen(port, () => {
 
