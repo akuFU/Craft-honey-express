989d0c2fdda82f473735e04d503d04082613e0c5
akuFU <nikita.akulenko95@gmail.com>
2023-02-23 13:33:47 +0600

    testing api fetch

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(bodyParser.json());
 app.get('/api/productsList', async (req,res) => {
 
 	let data = {};
+	console.log(req);
 	db.all("SELECT * FROM products", function(err, rows) {
 
 		rows.forEach((row) => data[row.id] = row);
