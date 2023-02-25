c326863f73f7bfc28a3b52875c10ae28e2f88a3a
akuFU <nikita.akulenko95@gmail.com>
2023-02-25 13:28:15 +0600

    reverting somechanges to keep it functioning before moving fully to ssr

--- app.js
+++ app.js
@@ -1,23 +1,57 @@
-const history = require('connect-history-api-fallback');
-const express = require('express');
-const path = require('path');
+import history from 'connect-history-api-fallback';
+import { createSSRApp } from 'vue';
+import { renderToString } from 'vue/server-renderer';
+import { fileURLToPath } from 'url';
+import { createServer as createViteServer } from 'vite';
+import fs from 'fs';
+import express from 'express';
+import path from 'path';
+import dot from 'dotenv';
 
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('testdb.db');
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
+console.log(__dirname);
+
+dot.config();
+let conString = process.env.CON_STRING;
+import pg from 'pg';
+const client = new pg.Client(conString);
+client.connect();
+
+import bodyParser from 'body-parser'
 const app = express(),
-	bodyParser = require("body-parser");
 	port = process.env.PORT || 3080;
 
 app.use(bodyParser.json());
 
+app.post('/api/language', (req, res) => {
+
+	let id = [req.body[1]];
+	console.log(id)
+	client.query("SELECT * FROM language WHERE id=$1", id)
+		.then(resp => { res.json(resp.rows[0]) })
+		.catch(err => { console.log(err) })
+
+});
+
+app.post('/api/languages', (req, res) => {
+
+	let id = [req.body[1]];
+	console.log(id)
+	client.query("INSERT INTO languages (id, lang), values ('en', $1)", id)
+		.then(resp => { console.log(resp) })
+		.catch(err => { console.log(err) })
+
+});
+
 app.get('/api/productsList', async (req,res) => {
 
 	let data = {};
-	console.log(req);
-	db.all("SELECT * FROM products", function(err, rows) {
-
-		rows.forEach((row) => data[row.id] = row);
+	client.query("SELECT * FROM products", function(err, rows) {
+	
+		for (let row of rows.rows) {
+			data[row.id] = row;
+		}
 		res.json(data);
 
 	}); 
@@ -27,17 +61,25 @@ app.get('/api/productsList', async (req,res) => {
 app.get('/api/productsListOpt', async (req,res) => {
 
 	let data = {};
-	db.all("SELECT * FROM productsOpt", function(err, rows) {
-
-		rows.forEach((row) => data[row.id] = row);
+	client.query("SELECT * FROM productsOpt", function(err, rows) {
+	
+		for (let row of rows.rows) {
+			data[row.id] = row;
+		}
 		res.json(data);
-
+		
 	}); 
 
 })
 
 app.use(history());
+
 app.use(express.static(path.join(__dirname, './Craft-honey/dist')));
+
+app.get('/', (req, res) => {
+
+
+});
 
 app.listen(port, () => {
 
